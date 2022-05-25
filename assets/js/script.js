d306988eea571f4f2eec7ac266195700a83d59c2
lundbmp <m.lundberg008@gmail.com>
2022-05-25 15:11:47 -0500

    adding quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
 let timeCount = 60;
+let questionArray = [{
+    question: "What is not a datatype in javascript?",
+    a1: "float",
+    a2: "number",
+    a3: "string",
+    a4: "boolean",
+    answer: "float"
+}, {
+    question: "Which is a semantic html element?",
+    a1: "<section>",
+    a2: "<span>",
+    a3: "<div>",
+    a4: "<p>",
+    answer: "<section>"
+} , {
+    question: "What do you enclose if else statements with?",
+    a1: "{ }",
+    a2: "[ ]",
+    a3: "< >",
+    a4: "( )",
+    answer: "{ }"
+} , {
+    question: "Arrays can be used to store: ",
+    a1: "objects",
+    a2: "numbers",
+    a3: "booleans",
+    a4: "all of the above",
+    answer: "all of the above"
+} , {
+    question: "How do you write to the console in javascript?",
+    a1: "document.write",
+    a2: "system.print",
+    a3: "console.log",
+    a4: "printf",
+    answer: "console.log"
+}];
 
 let testTimer = setInterval(function() {
     timeCount--;
@@ -9,3 +45,4 @@ let testTimer = setInterval(function() {
         // additional logic will go here to kill the quiz
     }
 }, 1000);
+
